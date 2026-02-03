53754179da570e7ddb2d338e81a646a165707968
Dhruv Sethi <githubdhruv590@gmail.com>
2026-02-03 22:28:43 +0530

    Added README as long_description for PyPI

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name="Topsis-Dhruv-102303785",
-    version="0.1",
+    version="0.2",
+    author="Dhruv Sethi",
+    description="TOPSIS implementation using Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
-    install_requires=["pandas", "numpy"],
+    install_requires=["numpy", "pandas"],
     entry_points={
         "console_scripts": [
             "topsis=topsis.topsis:main"
         ]
     },
-    author="Dhruv Sethi",
-    description="TOPSIS implementation using Python",
+    license="MIT",
 )
