8812bf181bf4d14797bd2c29853bd61e4dd1e6b6
Dhruv Sethi <githubdhruv590@gmail.com>
2026-02-03 19:03:03 +0530

    Updated package name as per assignment convention

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Topsis-Dhruv-10234567",
-
+    name="Topsis-Dhruv-102303785",
     version="0.1",
     packages=find_packages(),
     install_requires=["pandas", "numpy"],
